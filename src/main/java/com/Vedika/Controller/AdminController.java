17cab3058c6dedfcf3ef7e773a48bb57e7111e8a
Ratnesh Mishra <ratneshmishrarulz@gmail.com>
2023-03-10 16:31:00 +0530

    Added contacted user details page

--- src/main/java/com/Vedika/Controller/AdminController.java
+++ src/main/java/com/Vedika/Controller/AdminController.java
@@ -4,11 +4,12 @@ import com.Vedika.Model.CountVisitor;
 import com.Vedika.Service.AdminService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
@@ -19,6 +20,10 @@ public class AdminController {
         return adminService.getVisitorCount();
     }
 
+    @GetMapping("/user-details")
+    public String getUsersContacted() {
+        return "/admin/formsubmission.html";
+    }
 
 
 }
