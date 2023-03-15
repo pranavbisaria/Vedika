2864cf8e4228a3f07748df5ca119683369f1c405
Pranav Bisaria <pranavbisariya29@gmail.com>
2023-03-15 22:59:14 +0530

    Updated: Contact Form

--- src/main/java/com/Vedika/Model/Visitors.java
+++ src/main/java/com/Vedika/Model/Visitors.java
@@ -1,6 +1,8 @@
 package com.Vedika.Model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -16,17 +18,21 @@ public class Visitors {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotNull
     private String firstname;
     private String lastname;
-    private String address;
-    private String email;
+    @NotNull
     private Long phoneNumber;
-    private Date preferableDate;
-    private Date preferableTime;
+    private String email;
+    private String address;
+    private String sizeReq;
+    private Long quantityReq;
     private String enquiry;
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Product product;
     @CreatedDate
+    @JsonFormat(pattern = "dd-MM-yyyy")
     @Column(updatable = false, nullable = false)
     private Date createdDate;
+    @Column(length = 10000)
+    private String remarks;
+    private boolean actionTaken = false;
 }
--- src/main/java/com/Vedika/Payload/VisitorDto.java
+++ src/main/java/com/Vedika/Payload/VisitorDto.java
@@ -1,38 +1,46 @@
 package com.Vedika.Payload;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
 import java.util.Date;
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class VisitorDto {
+    @NotNull
     private String firstname;
     @Nullable
     private String lastname;
-    @Nullable
-    private String address;
+    @NotNull
+    @Pattern(regexp="^(\\+\\d{2}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$", message = "Not a valid number, please enter a 10 digit valid mobile number")
+    private String phoneNumber;
     @Email
     @Nullable
     private String email;
-    @Pattern(regexp="^(\\+\\d{2}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$", message = "Not a valid number, please enter a 10 digit valid mobile number")
-    private String phoneNumber;
     @Nullable
-    @JsonFormat(pattern = "dd-MM-yyyy")
-    private Date preferableDate;
+    private String address;
     @Nullable
-    @JsonFormat(pattern = "hh:mm")
-    private Date preferableTime;
+    private String sizeReq;
+    @Nullable
+    private Long quantityReq;
     @Nullable
     private String enquiry;
     @JsonFormat(pattern = "dd-MM-yyyy")
     private Date createdDate;
     @Nullable
-    private ProductDto product;
+    private String remarks;
+    private boolean actionTaken = false;
 }
